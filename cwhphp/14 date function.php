4baf4b37443dad44a18c179c4e13812507ac41bb
Durgesh <durgeshagrharibst1234@gmail.com>
2022-01-21 10:23:02 +0530

    All changes is done

---
+++ cwhphp/14 date function.php
@@ -0,0 +1,17 @@
+<?php
+echo "Welcome to the world of dates<br>";
+// $d = date("dS F Y");
+$d = date("dS F Y, g:i A");
+echo "Todays date is $d <br>";
+// Quick Quiz - Post at least 3 examples of dates in php in the comments below
+
+// Prints something like: Monday 8th of August 2005 03:12:46 PM
+echo date('l jS \of F Y h:i:s A');
+ 
+$year = date("Y");
+echo "<br>";
+echo "Copyright $year | All rights reserved <br>";
+
+?>
+
+
---
+++ cwhphp/20 database mysqli automatict create.php
@@ -0,0 +1,22 @@
+<?php
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Create a db
+$sql = "CREATE DATABASE dbHarry1";
+$result = mysqli_query($conn, $sql);
+
+// Check for the database creation success
+if($result){
+    echo "The db was created successfully!<br>";
+}
+else{
+    echo "The db was not created successfully because of this error ---> ". mysqli_error($conn);
+}
+  
+?>
